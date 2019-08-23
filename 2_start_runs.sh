c2acb702489f83b9a9cb328d43b82835bd0e72d5
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-08-23 15:10:48 -0500

    Documentation and other minor updates

--- 2_start_runs.sh
+++ 2_start_runs.sh
@@ -1,13 +1,13 @@
 source /opt/lsf9/conf/lsf.conf
 source config/project_config.sh
 
-
-
 CWL="$TD_ROOT/cwl/workflows/tindaisy.cwl"
+CQD="$TD_ROOT/src"
 
 # -J N - specify number of jobs to run at once
-#ARGS="-J 6"
-bash $TD_ROOT/src/rungo $ARGS -R $CROMWELL_JAR -c $CWL -C $CONFIG $@
+# -F - finalize and compress jobs immediately upon completion
+ARGS="-J 4 -F"
+bash $TD_ROOT/src/rungo $ARGS -c $CQD -R $CROMWELL_JAR -W $CWL -C $CONFIG_FILE $@
 
 rc=$?
 if [[ $rc != 0 ]]; then
