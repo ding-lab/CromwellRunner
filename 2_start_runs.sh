476fc77dd8c9426af82e3feb645a90900489e3e6
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-11-15 21:21:50 +0000

    Successful restart run

--- 2_start_runs.sh
+++ 2_start_runs.sh
@@ -1,7 +1,6 @@
 source /opt/lsf9/conf/lsf.conf
 source config/project_config.sh
 
-CWL="$TD_ROOT/cwl/workflows/tindaisy.cwl"
 CQD="$TD_ROOT/src"
 
 export DATALOG="/gscuser/mwyczalk/projects/TinDaisy/CromwellRunner/cq.datalog/datalog.dat"
@@ -9,7 +8,7 @@ export DATALOG="/gscuser/mwyczalk/projects/TinDaisy/CromwellRunner/cq.datalog/da
 # -J N - specify number of jobs to run at once
 # -F - finalize and compress jobs immediately upon completion
 #ARGS="-J 4 -F"
-ARGS="-F"
+#ARGS="-F"
 ARGS="$ARGS -X -Xmx10g"
 bash $TD_ROOT/src/rungo $ARGS -p $PROJECT -c $CQD -R $CROMWELL_JAR -W $CWL -C $CONFIG_FILE -k $CASES_FN $@
 
