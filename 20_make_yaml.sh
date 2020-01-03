fadbd202fb6cb902b1f5906c66ac1a59c5a4a130
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2020-01-02 22:34:51 -0600

    Preliminary implementation of SomaticSV run

--- 20_make_yaml.sh
+++ 20_make_yaml.sh
@@ -8,11 +8,10 @@
 # Note that running `runplan` with no arguments will give back useful information about anticipated runs
 # perhaps add this as default?
 
-RESTART_MAP="dat/LSCC.20191104.restart-map.dat"
 PARAMS="Project.config.sh"
 
 >&2 echo Writing YAML files
-CMD="src/runplan -x yaml -P $PARAMS -R $RESTART_MAP $@ "
+CMD="src/runplan -x yaml -P $PARAMS $@ "
 
 >&2 echo Running: $CMD
 eval $CMD
