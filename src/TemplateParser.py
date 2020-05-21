502944f3686a62f82371c3b1b74569caf0a83ccf
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-05-20 23:46:57 -0500

    Working version of TemplateParser.py

--- src/TemplateParser.py
+++ src/TemplateParser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# Python 2.7.12
 
 # Matthew Wyczalkowski
 # m.wyczalkowski@wustl.edu
@@ -11,9 +12,6 @@ def eprint(*args, **kwargs):
 # Portable printing to stderr, from https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python-2
     print(*args, file=sys.stderr, **kwargs)
 
-def check_arg(line, key_vals, arg.warn):
-    
-
 def main():
     import argparse
 
@@ -21,35 +19,49 @@ def main():
         Read a template file and key/value pairs, evaluate for mandatory keys, 
         and replace keys with values in template file.
 
-        Mandatory keys are defined in template as,
-            require_arg:key
-        If key is not specified, quit with error.
+        Mandatory keys are defined in template as, "# require_arg:key";
+        if key is not specified, quit with error.
         """
 
     parser = argparse.ArgumentParser(description=usage_text)
 
     parser.add_argument('kv', metavar='key:value', type=str, nargs='+', help='List of key:value pairs')
     parser.add_argument('-t', '--template', type=str, required=True, help='Template input filename.')
-    parser.add_argument('-o', '--output', type=str, help='Output filename. Create output dir if necessary. Default write to stderr')
-    parser.add_argument('-o', '--debug', action="store_true", default=False, help='Print debugging information to stderr')
-    parser.add_argument('-w', '--warn', action="store_true", default=False, help='Warn don\'t quit if required argument missing')
+    parser.add_argument('-o', '--output', type=str, default="stdout", help='Output filename. Create output dir if necessary. Default write to stderr')
+    parser.add_argument('-d', '--debug', action="store_true", default=False, help='Print debugging information to stderr')
+#    parser.add_argument('-w', '--warn', action="store_true", default=False, help='Warn don\'t quit if required argument missing')
 
     args = parser.parse_args()
 
+    key_vals={}
     # convert kv to key_vals
+    for kv in args.kv:
+        t = kv.split(':')
+        if len(t) != 2:
+            eprint("ERROR: bad format of key:value pair %s" % kv)
+        key_vals[t[0]] = t[1] 
+        if args.debug:
+            eprint("Key = %s Value = %s" % (t[0], t[1]))
 
-    if args.outfn == "stdout":
+    if args.output == "stdout":
         o = sys.stdout
+        if args.debug:
+            eprint("Writing to stdout")
     else:
-        o = open(argargs.outfn, "w")
+        o = open(args.output, "w")
+        if args.debug:
+            eprint("Writing to %s" % args.output)
 
     with open(args.template) as f:
         for line in f:
+            line=line.rstrip()
+#            if args.debug:
+#                eprint("Processing", line)
             # check to see if this line specifies required tokens
             req_token="require_arg:"
             if req_token in line:
-                req_key=line[line.index(req_token)+len(req_token):]
-                if debug:
+                req_key=line[line.index(req_token)+len(req_token):].strip()
+                if args.debug:
                     eprint("Checking for required key %s" % req_key)
                 if req_key not in key_vals.keys():
                     eprint("ERROR: required key %s not found" % req_key)
@@ -57,8 +69,8 @@ def main():
             # this is a regular line.  Replace keys with values                    
             else:
                 for k, v in key_vals.iteritems():
-                    line = line.replace(i, j)
-                    o.write(line)
+                    line = line.replace(k, v)
+                o.write(line + "\n")
 
     o.close()
 
