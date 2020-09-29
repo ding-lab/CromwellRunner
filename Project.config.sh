91482c679bf6fd0bae78ef6442333313f3f85f4c
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-09-29 16:36:41 -0500

    Adding mode changes which compute1 imposes
    * Don't really understand why mode changes, may be a compute1 effect

--- Project.config.sh
+++ Project.config.sh
@@ -1,19 +1,14 @@
-#
+##
 # Project config
 #
 
-PROJECT="UCEC.20200909"
+PROJECT="03.WGS_CNV_Somatic.Y3.208"
 
-SYSTEM_CONFIG="config/Definitions/System/MGI.gc2541.config.sh"
-COLLECTION_CONFIG="config/Definitions/Collection/CPTAC3-GRCh38.TinDaisy.config.sh"
-WORKFLOW_CONFIG="config/Definitions/Workflow/TinDaisy-hotspot.config.sh"
-
-# This should probably be moved here, currently it is in WORKFLOW_CONFIG
-YAML_TEMPLATE="config/Templates/YAML/tindaisy-hotspot.template.yaml"
+SYSTEM_CONFIG="config/Definitions/System/compute1.SomaticSV.config.sh"
+COLLECTION_CONFIG="config/Definitions/Collection/CPTAC3-GRCh38.config.sh"
+WORKFLOW_CONFIG="config/Definitions/Workflow/SomaticSV.config.sh"
 
 # source them all in order
 source $SYSTEM_CONFIG
 source $COLLECTION_CONFIG
 source $WORKFLOW_CONFIG
-
-
