c6282c1bd9c9563c6af061c97ddfb6a08b7de777
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-09-29 16:46:01 -0500

    Adding modifications from prior compute1 SomaticSV run 02.WGS_SV.Y3.620
    * compute1:/home/m.wyczalkowski/Projects/CromwellRunner/SomaticSV/02.WGS_SV.Y3.620
    * modifications were made specific to runs on compute1 but were not committed.  Doing so manually

---
+++ 60_move_results.sh
@@ -0,0 +1,46 @@
+
+# Need to move all data from scratch volume to storage volume
+# Also, update dat/analysis_summary.dat file
+# write dat/analysis_summary.unstaged.dat
+
+#SCRATCH_ROOT=/scratch1/fs1/dinglab/m.wyczalkowski/cromwell-data
+SCRATCH_BASE=/scratch1/fs1/dinglab/m.wyczalkowski/cromwell-data/cromwell-workdir/cromwell-executions/SomaticSV.cwl
+#DEST_ROOT=/storage1/fs1/m.wyczalkowski/Active/cromwell-data
+DEST_BASE=/storage1/fs1/m.wyczalkowski/Active/cromwell-data/cromwell-workdir/cromwell-executions/SomaticSV.cwl
+
+
+function test_exit_status {
+    # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+    rcs=${PIPESTATUS[*]};
+    for rc in ${rcs}; do
+        if [[ $rc != 0 ]]; then
+            >&2 echo Fatal error.  Exiting.
+            exit $rc;
+        fi;
+    done
+}
+
+# We will move all relevant $SCRATCH_BASE/UUID directories to $DEST_BASE
+
+AS="dat/analysis_summary.dat"
+AS_DEST="dat/analysis_summary.final.dat"
+
+UUIDS=$(grep "$SCRATCH_BASE" $AS | cut -f 9 | sort -u)
+
+for UUID in $UUIDS ; do
+    echo Processing $UUID
+
+    CMD="mv $SCRATCH_BASE/$UUID $DEST_BASE"
+    >&2 echo $CMD
+    eval $CMD
+    test_exit_status
+done
+
+CMD="sed \"s+$SCRATCH_BASE+$DEST_BASE+\" $AS > $AS_DEST"
+>&2 echo $CMD
+eval $CMD
+
+>&2 echo Moved data from $SCRATCH_BASE
+>&2 echo to $DEST_BASE
+
+>&2 echo Analysis summary written to $AS_DEST
--- config/Definitions/System/compute1.SomaticSV.config.sh
+++ config/Definitions/System/compute1.SomaticSV.config.sh
@@ -5,9 +5,9 @@
 
 SYSTEM="compute1"  
 LSF_CONF="/opt/ibm/lsfsuite/lsf/conf/lsf.conf"
-LSF_GROUP="/mwyczalk/gdc-download"
+LSF_GROUP="/m.wyczalkowski/cromwell-runner"
 LSFQ="general"              # for MGI, queue is "research-hpc"
-LSF_ARGS="-g $LSF_GROUP -M -q $LSFQ"
+LSF_ARGS="-B \"-g $LSF_GROUP\"  -M -q $LSFQ"
 
 
 # This is based on CromwellRunner
@@ -15,7 +15,10 @@ CROMWELL_JAR="/usr/local/cromwell/cromwell-47.jar"
 
 # Workflow root - where Cromwell output goes.  This value replaces text WORKFLOW_ROOT in CONFIG_TEMPLATE,
 # and is written to CONFIG_FILE
-WORKFLOW_ROOT="/storage1/fs1/m.wyczalkowski/Active/cromwell-data"
+#WORKFLOW_ROOT="/storage1/fs1/m.wyczalkowski/Active/cromwell-data"
+
+# Writing to scratch
+WORKFLOW_ROOT="/scratch1/fs1/dinglab/m.wyczalkowski/cromwell-data"
 CONFIG_TEMPLATE="config/Templates/cromwell-config/cromwell-config-db.compute1.template.dat"
 
 # Path to BamMap, which is a file which defines sequence data path and other metadata
@@ -25,12 +28,19 @@ BAMMAP="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog/BamMa
 # Assume that all references are based here
 REF_ROOT="/storage1/fs1/dinglab/Active/Resources/References"
 
-# CWL_ROOT is needed for CWL
+# CWL_ROOT is needed for CWL.  It is the container path to where project is installed
 CWL_ROOT="/home/m.wyczalkowski/Projects/SomaticSV"
+#CWL_ROOT="/usr/local/SomaticSV"
 
 # Using common datalog file
 export DATALOG="/storage1/fs1/m.wyczalkowski/Active/cromwell-data/CromwellRunner/datalog.dat"
 
 # All these volumes will be mounted, with paths in container same as on host
-# Note that /home is expanded to /storage1/fs1/home1/Active/home
-VOLUME_MAPPING="/storage1/fs1/m.wyczalkowski/Active /storage1/fs1/dinglab/Active /storage1/fs1/home1/Active/home/m.wyczalkowski:/home/m.wyczalkowski"
+# Note that /home used to be expanded to /storage1/fs1/home1/Active/home
+# however,  we no longer map home
+VOLUME_MAPPING=" \
+/storage1/fs1/m.wyczalkowski/Active \
+/storage1/fs1/dinglab/Active \
+/storage1/fs1/home1/Active/home/m.wyczalkowski:/home/m.wyczalkowski \
+/scratch1/fs1/dinglab/m.wyczalkowski/cromwell-data"
+
