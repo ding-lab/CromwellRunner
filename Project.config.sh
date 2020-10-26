05584f4801a51b61cd5b3c5968bc64ea49e925f9
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2020-10-26 15:15:20 +0000

    Successful run with cleanup of tindaisy2 on MGI

--- Project.config.sh
+++ Project.config.sh
@@ -2,18 +2,15 @@
 # Project config
 #
 
-PROJECT="03.WGS_CNV_Somatic.Y3.208"
+PROJECT="13_CCRCC4.TD2-test"
 
-#SYSTEM_CONFIG="config/Definitions/System/compute1.SomaticSV.config.sh"
-#COLLECTION_CONFIG="config/Definitions/Collection/CPTAC3-GRCh38.config.sh"
-#WORKFLOW_CONFIG="config/Definitions/Workflow/SomaticSV.config.sh"
-#
-## source them all in order
-#source $SYSTEM_CONFIG
-#source $COLLECTION_CONFIG
-#source $WORKFLOW_CONFIG
+# From ../12
+#SYSTEM_CONFIG="config/Definitions/System/MGI.gc2541.config.sh"
+#COLLECTION_CONFIG="config/Definitions/Collection/CPTAC3-GRCh38.TinDaisy.config.sh"
+#WORKFLOW_CONFIG="config/Definitions/Workflow/TinDaisy-hotspot.config.sh"
+
+#The above were used to create merged config file below
 
 # Merged config
-
-MERGED_CONFIG="config/Definitions/MergedDefinitions/compute1.SomaticSV.CPTAC3-GRCh38.sh"
+MERGED_CONFIG="config/Definitions/MergedDefinitions/MGI.gc2541.TinDaisy2.CPTAC3-GRCh38.sh"
 source $MERGED_CONFIG
