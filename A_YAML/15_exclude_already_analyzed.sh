c9a19dda4ca6f7a3de642342a679db62eb25c076
Wyczalkowski <m.wyczalkowski@compute1-exec-88.ris.wustl.edu>
2021-10-14 17:17:27 +0000

    Updates setting up SomaticCNV runs

--- A_YAML/15_exclude_already_analyzed.sh
+++ A_YAML/15_exclude_already_analyzed.sh
@@ -4,11 +4,18 @@ cd ..
 
 source Project.config.sh
 
-DCC="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog/DCC_Analysis_Summary/WGS_SV.DCC_analysis_summary.dat"
-RUN_LIST="dat/RUN_LIST.dat"
+# SomaticSV
+#DCC="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog/DCC_Analysis_Summary/WGS_SV.DCC_analysis_summary.dat"
+#TUMOR_COL=12
+
+# SomaticCNV 
+DCC="/storage1/fs1/dinglab/Active/Projects/CPTAC3/Common/CPTAC3.catalog/DCC_Analysis_Summary/WGS_CNV_Somatic.DCC_analysis_summary.dat"
+TUMOR_COL=12
+
+RUN_LIST="dat/RUN_LIST.142.dat"
 OUTA="./dat/tumor_uuid_to_run.dat"
 
-CMD="comm -23 <(cut -f 3 $RUN_LIST | sort) <(cut -f 12 $DCC | grep -v tumor_uuid | sort) > $OUTA"
+CMD="comm -23 <(cut -f 3 $RUN_LIST | sort) <(cut -f $TUMOR_COL $DCC | sort) > $OUTA"
 >&2 echo Running: $CMD
 eval $CMD
 
@@ -17,4 +24,6 @@ RUN_LIST_NEW="dat/RUN_LIST.refined.dat"
 
 grep -f $OUTA $RUN_LIST > $RUN_LIST_NEW
 >&2 echo Written to $RUN_LIST_NEW
+
+
 
