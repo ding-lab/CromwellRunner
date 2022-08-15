e14f8273d7888c3ece25f5d149df55961ff9e299
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2022-08-15 10:16:38 -0500

    Incremental updates, SomaticCNV to run with catalog3 and run list 9

--- A_YAML/20_make_yaml.sh
+++ A_YAML/20_make_yaml.sh
@@ -19,18 +19,21 @@
 #    datafile2_uuid
 # Also, RUNLIST9 has a header line while RUNLIST4 does not
 
+# Usage: 20_make_yaml.sh dat/WGS_CNV_Somatic.run_list.tsv
 
 cd ..
 PARAMS="Project.config.sh"
 source $PARAMS
 
-RL9="dat/WXS_Somatic_Variant_TD.run_list.tsv"
+#RL9="dat/WGS_CNV_Somatic.run_list.tsv"
+RL9=$1
+
 if [ -z $RUN_LIST ]; then
     >&2 echo ERROR: RUN_LIST parameter not defined in $PARAMS
     exit 1
 fi
 if [ ! -e $RL9 ]; then
-    >&2 echo ERROR: $RL9 not found
+    >&2 echo ERROR: RL9 $RL9 not found
     exit 1
 fi
 
