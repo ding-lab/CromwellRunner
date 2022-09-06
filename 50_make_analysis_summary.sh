28e85a693f2ce54de20b75a2294489bcdf8ec0e3
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2022-09-06 16:18:47 -0500

    Adding test for excess zeros, specific only to Somatic CNV, to detect error condition.

--- 50_make_analysis_summary.sh
+++ 50_make_analysis_summary.sh
@@ -18,6 +18,14 @@ else
 fi
 CMD="bash src/summarize_runs.sh $@ -s $AS_NAME -B $BAMMAP -P $PARAMS -U $RUN_LIST -C $CATALOG"
 
+# This is specific to a particular flag thrown by WGS SomaticCNV pipeline.  Should be a more general way to write
+if grep -q excess_zero $AS_NAME; then
+    >&2 echo WARNING: Excess zeros observed in $AS_NAME:
+    grep -q excess_zero $AS_NAME
+    >&2 echo Quitting 
+    exit 1
+fi
+
 >&2 echo Running: $CMD
 eval $CMD
 
